69f5e2b4c83d25efe04fa8c410f35b9f2ea51cfd
Juri <juri.dispan@me.com>
2017-08-11 14:39:17 +0200

    Update screxec.sh

--- screxec.sh
+++ screxec.sh
@@ -1,17 +1,22 @@
-echo -e "\e[31mHello guys this is my wonderful update script\e[0m"
+echo Hello guys this is my wonderful update script
 
-#Do what you want to do
+###########################
+###Do what you want to do##
+###########################
+
+#update everything
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get clean
 sudo apt-get -y autoremove
 
-
 #update this file
 cd ~/screxec
 echo now updating this file
 wget https://raw.githubusercontent.com/juril33t/scr-exec/master/screxec.sh
 wget https://raw.githubusercontent.com/juril33t/scr-exec/master/rename.sh
+
+mv screxec.sh.1 screxec.sh
 mv rename.sh.1 rename.sh
-sh rename.sh
+#sh rename.sh
