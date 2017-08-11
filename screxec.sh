623c7de582a8ed31d36aeeeea77fe2c2193ba976
Juri <juri.dispan@me.com>
2017-08-11 14:33:56 +0200

    Update screxec.sh

--- screxec.sh
+++ screxec.sh
@@ -1,4 +1,11 @@
+echo -e "\e[31mHello guys this is my wonderful update script\e[0m"
+
 #Do what you want to do
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get clean
+sudo apt-get -y autoremove
 
 
 #update this file
