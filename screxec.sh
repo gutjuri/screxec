551a83532f257ec1eea04e9006c5d209a928def4
Juri <juri.dispan@me.com>
2017-08-11 14:23:26 +0200

    Update screxec.sh

--- screxec.sh
+++ screxec.sh
@@ -6,4 +6,4 @@ echo now updating this file
 wget https://raw.githubusercontent.com/juril33t/scr-exec/master/screxec.sh
 wget https://raw.githubusercontent.com/juril33t/scr-exec/master/rename.sh
 mv rename.sh.1 rename.sh
-sh rename
+sh rename.sh
