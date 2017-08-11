10f61637d7b5e6a41912968a6d7338b9484edbad
Juri <juri.dispan@me.com>
2017-08-11 15:16:14 +0200

    Update screxec.sh

--- screxec.sh
+++ screxec.sh
@@ -4,6 +4,7 @@ echo Hello guys this is my wonderful update script
 
 #update everything
 update() {
+  echo now updating everything
   sudo apt-get update
   sudo apt-get -y upgrade
   sudo apt-get -y dist-upgrade
@@ -15,10 +16,11 @@ update() {
 ###Do what you want to do HERE##
 ################################
 
-#update
+update
 
 #update this file
 cd ~/screxec
 echo now updating this file
 wget https://raw.githubusercontent.com/juril33t/scr-exec/master/screxec.sh
 mv screxec.sh.1 screxec.sh
+echo update successful
