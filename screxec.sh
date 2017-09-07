dd340c72a316cf85fa4763604b1b73d59d85c7ca
Juri <juri.dispan@me.com>
2017-09-07 12:40:15 +0200

    Update screxec.sh

--- screxec.sh
+++ screxec.sh
@@ -2,7 +2,7 @@
 
 echo Hello guys this is my wonderful update script
 
-#update everything
+# update everything
 update() {
   echo now updating everything
   sudo apt-get update
@@ -11,17 +11,29 @@ update() {
   sudo apt-get clean
   sudo apt-get -y autoremove
 }
- 
-################################
-###Do what you want to do HERE##
-################################
 
-update
+# update this file
+updatefile() {
+  cd ~/screxec
+  echo now updating this file
+  wget https://raw.githubusercontent.com/juril33t/scr-exec/master/screxec.sh
+  mv screxec.sh.1 screxec.sh
+  echo update successful
+}
 
-#update this file
-cd ~/screxec
-echo now updating this file
-wget https://raw.githubusercontent.com/juril33t/scr-exec/master/screxec.sh
-mv screxec.sh.1 screxec.sh
-echo update successful
-sudo reboot
+# check if online
+# if online, do an update
+# else wait for internet to come back
+
+wget -q --tries=10 --timeout=20 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+  update
+  updatefile
+  sudo reboot
+else
+  sleep 10m
+  sh screxec.sh
+fi
+
+
+
